1b9f7bb9130d996f1bbca647d6b39597ecc017d0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-14 08:45:07 -0700

    created the connection to my database via mysql2

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 require("dotenv").config();
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    // livehost
+    host: "localhost",
+    // MySQL username,
+    user: DB_USER,
+    // MySQL password
+    password: DB_PASSWORD,
+    // Database name
+    database: DB_NAME,
+  },
+  console.log(`Connected to the employee_db database.`)
+);
