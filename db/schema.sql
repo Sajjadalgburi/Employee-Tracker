afac05cdfe8b2c29906a963b8b2d1d982017788f
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-14 09:04:17 -0700

    Created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -14,8 +14,18 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for the role
     title VARCHAR(30) NOT NULL, -- To hold role title
-    salary DECIMAL NOT NULL, -- Salary for the respective employee role 
+    salary DECIMAL(10, 2) NOT NULL, -- Salary for the respective employee role 
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id) -- Hold's reference to department role that it belongs to
+);
+
+-- Creating employee table
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for the employee
+    first_name VARCHAR(30) NOT NULL, -- To hold employee first name
+    last_name VARCHAR(30) NOT NULL, -- To hold employee last  name
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id), -- To hold reference to employee role
+    manager_id INT NULL, -- Employee's can have managers but this filed can still be null if no manager is assigned
 );
 
