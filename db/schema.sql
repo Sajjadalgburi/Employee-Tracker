1af57c4b90087c3c735326e5da3418edc39bf865
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-14 08:51:04 -0700

    created the department table

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,10 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+-- Creating department table
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for the department
+    name VARCHAR(30) NOT NULL, -- Name column for department
+)
+
