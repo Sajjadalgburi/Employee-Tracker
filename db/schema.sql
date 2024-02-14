718bab05339fa22693407e9a1da755f339af7b47
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-14 08:56:25 -0700

    Created role table

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,14 @@ USE employee_db;
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for the department
     name VARCHAR(30) NOT NULL, -- Name column for department
-)
+);
+
+-- Creating role table
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for the role
+    title VARCHAR(30) NOT NULL, -- To hold role title
+    salary DECIMAL NOT NULL, -- Salary for the respective employee role 
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) -- Hold's reference to department role that it belongs to
+);
 
